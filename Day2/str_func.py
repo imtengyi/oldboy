4bcc998e1ed58ecb2c04987e3f13cf5df4661ffd
dmmjy9@live.cn <dmmjy9@live.cn>
2016-09-24 12:11:44 +0800

    Day_2 sync.

--- Day2/str_func.py
+++ Day2/str_func.py
@@ -1,10 +1,23 @@
-name = str("qwertyqwerty")
-print (type(str))
-print (dir(str))
+#!/usr/bin/env python3
 
-print(name.count("q"))
-print(name.center(50,'*'))
-print(name.capitalize())
-print(name.encode("gbk"))
-print(name.endswith("ty"))
-print(name.__contains__("er"))
+# name = str("qwertyqwerty")
+# print (type(str))
+# print (dir(str))
+#
+# print(name.count("q"))
+# print(name.center(50,'*'))
+# print(name.capitalize())
+# print(name.encode("gbk"))
+# print(name.endswith("ty"))
+# print(name.__contains__("er"))
+
+def displayNumType(num):
+    print (num, 'is',end='')
+    if isinstance(num,(int, float, complex)):
+        print ('a number of type:',type(num).__name__)
+    else:
+        print ('not a number at all.')
+
+displayNumType(-69)
+displayNumType(99999999999999999999999999999)
+displayNumType(98.6)
