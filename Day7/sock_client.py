f674e3c939679cf98d12a1001c33cf9f7240a74e
dmmjy9 <dmmjy9@live.cn>
2016-10-20 18:34:22 +0800

    Day7_socket

---
+++ Day7/sock_client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+__auther__ = 'dmmjy9'
+
+import socket
+
+ip_port = ('127.0.0.1',8888)
+
+sk = socket.socket()
+sk.connect(ip_port)
+
+sk.sendall(bytes('client sent','utf-8'))
+
+server_reply = sk.recv(1024)
+print(server_reply)
+
+sk.close()
---
+++ Day7/sock_server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+__auther__ = 'dmmjy9'
+
+import socket
+
+ip_port = ('127.0.0.1',8888)
+
+sk = socket.socket()
+sk.bind(ip_port)
+sk.listen(5)
+
+while True:
+	print("server waiting...")
+	conn,addr = sk.accept()
+
+	client_data = conn.recv(1024)
+	print(str(client_data,'utf-8'))
+	conn.sendall(bytes('server sent','utf-8'))
+
+	conn.close()
