5e2776e31edecb14d61757b0022c02bfd848c3c0
dmmjy9 <dmmjy9@live.cn>
2016-10-21 14:27:52 +0800

    Day_7 tmp

--- test_1.py
+++ test_1.py
@@ -12,6 +12,11 @@ while True:
 	print("server waiting...")
 	conn,addr = sk.accept()
 	client_data = conn.recv(1024)
+	conn.sendall(bytes('connection succeed','utf-8'))
 	print(str(client_data,'utf-8'))
-	conn.sendall(bytes('server reply','utf-8'))
+	while True:
+		client_data = conn.recv(1024)
+		print(str(client_data,'utf-8'))
+		server_input = input("->>")
+		conn.sendall(bytes(server_input,'utf-8'))
 	conn.close()
