ed8cc73ed06eff1bcad42f3fa8b039eb7983fe32
dmmjy9@live.cn <dmmjy9@live.cn>
2016-10-21 17:01:09 +0800

    Day7_largeFile

--- Day7/sock_client.py
+++ Day7/sock_client.py
@@ -8,13 +8,27 @@ ip_port = ('127.0.0.1',8888)
 sk = socket.socket()
 sk.connect(ip_port)
 
-sk.sendall(bytes('client sent','utf-8'))
-
-server_reply = sk.recv(1024)
-print(server_reply)
 while True:
-    user_input = input(">>:").strip()
+    user_input = input("cmd:").strip()
+    if len(user_input) == 0:continue
+    if user_input == 'q':break
+
     sk.send(bytes(user_input,'utf-8'))
-    server_reply = sk.recv(1024)
-    print(str(server_reply,'utf-8'))
+    #ack_msg = b"CMD_RESULT_SIZE|%s"%len(cmd_result)
+    server_ack_msg = sk.recv(100)
+    cmd_res_msg = str(server_ack_msg.decode()).split("|")
+    if cmd_res_msg[0] == 'CMD_RESULT_SIZE':
+        cmd_res_size = int(cmd_res_msg[1])
+        sk.send(bytes("CLIENT_READY_TO_RECV","utf-8"))
+
+    res = ''
+    received_size = 0
+    while received_size < cmd_res_size:
+        data = sk.recv(500)
+        received_size += len(data)
+        res += str(data.decode())
+    else:
+        print(str(res))
+        print('---------recv done---------')
+
 sk.close()
--- Day7/sock_server.py
+++ Day7/sock_server.py
@@ -2,6 +2,7 @@
 __auther__ = 'dmmjy9'
 
 import socket
+import subprocess
 
 ip_port = ('127.0.0.1',8888)
 
@@ -13,12 +14,18 @@ while True:
 	print("server waiting...")
 	conn,addr = sk.accept()
 
-	client_data = conn.recv(1024)
-	print(str(client_data,'utf-8'))
-	conn.sendall(bytes('server sent','utf-8'))
 	while True:
 		client_data = conn.recv(1024)
-		print(str(client_data,'utf-8'))
-		server_response = input("\033[31:1m>>:\033[0m").strip()
-		conn.send(bytes(server_response,'utf-8'))
+		if not client_data:break
+		print("recv:",str(client_data,'utf-8'))
+		cmd = str(client_data,'utf-8').strip()
+		cmd_call = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+		cmd_result = cmd_call.stdout.read()
+		if len(cmd_result) == 0:
+			cmd_result = b"cmd execution has no output..."
+		ack_msg = bytes('CMD_RESULT_SIZE|%s'%len(cmd_result),'utf-8')
+		conn.send(ack_msg)
+		client_ack = conn.recv(50)
+		if client_ack.decode() == 'CLIENT_READY_TO_RECV':
+			conn.send(cmd_result)
 	conn.close()
